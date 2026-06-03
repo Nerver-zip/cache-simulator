5b012719c3c1c52effdcc5a211cca0180432608c
Nerver-zip <marceloaugusto12161@gmail.com>
2026-06-02 23:36:58 -0300

    feat(cache): Implementação da cache Random
    - Integrados os testes para a cache random. Todos estão passando com uma
    tolerância de no máximo ±1.5%

--- src/caches/Random.cpp
+++ src/caches/Random.cpp
@@ -1,9 +1,39 @@
 #include "Random.h"
 
-Random::Random(const CacheConfig& config){
+#include "Random.h"
 
-}
+Random::Random(const CacheConfig &config) : 
+    gen(std::random_device{}()), matrix(config.nsets), 
+    capacity(config.assoc), total_capacity(config.nsets * config.assoc), 
+    used_capacity(), dist(0, config.assoc - 1) {}
 
 bool Random::execute(uint32_t index, uint32_t tag){
+    auto &row = matrix[index];
+
+    for (int i = 0; i < (int)row.size(); i++)
+    {
+        if(row[i] == tag){
+            ++hits;
+            return true;
+        }
+    }
+
+    misses.total++;
+
+    if((int)row.size() == capacity){
+        row[dist(gen)] = tag;
+        if(used_capacity == total_capacity){
+            ++misses.capacity;
+        }
+        else{
+            ++misses.conflict;
+        }
+    }
+    else{
+        row.push_back(tag);
+        ++used_capacity;
+        ++misses.compulsory;
+    }
+
     return false;
 }
--- src/caches/Random.h
+++ src/caches/Random.h
@@ -1,20 +1,20 @@
 #pragma once
 
 #include "ICache.h"
-#include <cstdint>
 #include <vector>
+#include <random>
 
 class Random : public ICache {
 public:
     Random(const CacheConfig& config);
 
     bool execute(uint32_t index, uint32_t tag) override;
-    
+
 private:
-    struct CacheSet {
-        const int capacity;
-        std::vector<int> bucket;
-        
-        CacheSet(int capacity) : capacity(capacity), bucket(capacity) {}
-    };
+    std::mt19937 gen;
+    std::vector <std::vector<uint32_t >> matrix;
+    const int capacity;
+    const int total_capacity;
+    int used_capacity;
+    std::uniform_int_distribution<int> dist;
 };
